5129f16de3c491261a70b740c53b4744cc4c344c
Krishna <geekykicha87@gmail.com>
2024-12-30 08:29:15 +0000

    js added

---
+++ 04_Projects/script.js
@@ -0,0 +1,37 @@
+const randomNumber = parseInt(Math.random() * 100 +1);
+
+const submit = document.querySelector('#subt');
+const userInput = document.querySelector('#guessField');
+const guessSlot = document.querySelector('.guesses');
+const remaining = document.querySelector('lastResult');
+const LowOrHi = document.querySelector('.LowOrHi');
+const startOver = document.querySelector('.resultParas');
+
+const p = document.createElement('p');
+
+let prevGuess = []
+let numGuess = 1
+
+let playGame = true
+
+function validatGuess(guess) {
+    
+}
+
+function checkGuess(guess) {
+    
+}
+function displayGuess(guess) {
+    
+}
+function displayMessage(message) {
+    
+}
+
+function endGame(){
+
+}
+
+function newGame(){
+    
+}
